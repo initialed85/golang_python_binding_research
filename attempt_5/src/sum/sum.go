a4a5a0ced6085bdc15c8f86ed6a67aafe5271a6c
Edward Beech <initialed85@gmail.com>
2017-11-13 21:32:33 +0800

    He's done it some more

--- attempt_5/src/sum/sum.go
+++ attempt_5/src/sum/sum.go
@@ -1,14 +1,21 @@
 package sum
 
-import "errors"
+import (
+	"errors"
+	"time"
+	"sync"
+	"log"
+)
 
 type SomeObject struct {
-	Name string
+	Name   string
+	Things []string
 }
 
 func NewSomeObject(name string) SomeObject {
 	someObject := SomeObject{
-		Name: name,
+		Name:   name,
+		Things: []string{"hi", "how", "are", "you"},
 	}
 
 	return someObject
@@ -22,3 +29,29 @@ func Sum(a, b int) (float64, error) {
 	return float64(a) + float64(b), nil
 }
 
+func DoThing() int {
+	things := []int{1,2,3,4,5,6,76,567,35476,5436,534}
+	var locks = []sync.Mutex{}
+	for range things {
+		leChan := sync.Mutex{}
+		leChan.Lock()
+		locks = append(locks, leChan)
+		go meDoingTheThing(leChan)
+	}
+
+	time.Sleep(5*time.Millisecond)
+
+	for _, lock := range locks {
+		lock.Lock()
+		lock.Unlock()
+	}
+
+	return 1
+}
+
+func meDoingTheThing(blap sync.Mutex) {
+	log.Println("Doing thing")
+	<-time.After(5*time.Second)
+	blap.Unlock()
+	log.Println("No longer doing thing")
+}
--- attempt_6/build.sh
+++ attempt_6/build.sh
@@ -1,1 +1,33 @@
 #!/usr/bin/env bash
+
+# export GOROOT=/usr/local/Cellar/go/1.5.4
+# echo "GOROOT is $GOROOT"
+# echo ""
+
+export GOPATH=`pwd`
+echo "GOPATH is $GOPATH"
+echo ""
+
+export GO=`which go`
+echo "GO is $GO"
+echo ""
+
+GOVERSION=`$GO version`
+if [[ $GOVERSION != *"go1.9"* ]]; then
+    echo "error: Go version is not 1.9 (was $GOVERSION)"
+    exit 1
+fi
+
+# echo "getting gopy"
+# $GO get -v github.com/go-python/gopy
+# echo ""
+
+PYTHONPATH="`pwd`/src/github.com/go-python/gopy/"
+
+# echo "building gopy"
+# $GO build -a github.com/go-python/gopy
+# echo ""
+
+echo "build gosnmp_python bindings"
+./gopy bind -lang="py2" -output="attempt_6" github.com/initialed85/attempt_6
+echo ""
